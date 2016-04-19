1bb8754dffd40e6cee4ce0466349f02bdfa93f17
Buster Kaas Jensen <busterkaas@gmail.com>
2016-04-19 10:39:32 +0200

    first commit

---
+++ src/Logic/EV3Client.java
@@ -0,0 +1,55 @@
+package Logic;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EV3Client {
+
+	DataInputStream dis;
+	DataOutputStream dos;
+	boolean done;
+
+	public EV3Client(String serverIpAddress) {
+		try {
+			System.out.println("Connecting to EV3...");
+			Socket s = new Socket(serverIpAddress, 5000);
+
+			dis = new DataInputStream(s.getInputStream());
+			dos = new DataOutputStream(s.getOutputStream());
+
+			done = false;
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public void sendMessage(String message) {
+		try {
+			dos.writeUTF(message);
+			dos.flush();
+
+			try {
+				if (dis.available() > 0) {
+					String ev3Message = dis.readUTF();
+					System.out.println("ev3 response: " + ev3Message);
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+			String ev3Message = dis.readUTF();
+			System.out.println("EV3 says: " + ev3Message);
+
+			if (message.equalsIgnoreCase("quit")) {
+				done = true;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/UI/Main.java
@@ -0,0 +1,9 @@
+package UI;
+
+import Logic.EV3Server;
+
+public class Main {
+public static void main(String[] args) {
+	EV3Server server = new EV3Server("100.81.132.96", 6000);
+}
+}
